19a83249c052ab359968502d5badfd2b8923e0de
ryanrodrigu <97711855+ryanrodrigu@users.noreply.github.com>
2022-05-18 20:49:23 -0400

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,48 @@
+img = "";
+status ="";
+objects = [];
+function preload(){
+    img = loadImage('bedroom.jpeg');
+
+}
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetecter=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML ="Status : Detecting Objects";
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    if(status != "")
+    {
+        for (i = 0; i< objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " "+percent+"%", objects[i].x +15, objects[i].y +15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status= true;
+    objectDetecter.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
